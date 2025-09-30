075fff6a8c23f7b27a6c4a7c81aa980375a025bb
mohamed <feituri4456@gmail.com>
2025-09-29 21:29:40 -0600

    gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/whistle-site/',
+  base: '/Portfolio-website/',
   output: 'static',
   integrations: [tailwind()],
 });
